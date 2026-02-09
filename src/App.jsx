0f5b481eb92aa8da1c5a692017bc4b832c59df30
Adam Myers <adammyers002@gmail.com>
2026-02-08 23:13:45 -0600

    setup components and their routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [mailboxes, setMailBoxes] = useState([]);
 
-  return <></>;
+  const addBox = (formData) => {
+    const newMailBox = {
+      ...formData,
+      _id: mailboxes.lengh + 1,
+    };
+    setMailBoxes({ ...mailboxes, newMailBox });
+  };
+
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Post Office</h1>
+            </main>
+          }
+        />
+
+        <Route
+          path="/mailboxes"
+          element={<MailboxList mailboxes={mailboxes} />}
+        />
+
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+
+        <Route
+          path="/mailboxes/:mailboxId"
+          element={<MailboxDetails mailboxes={mailboxes} />}
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
